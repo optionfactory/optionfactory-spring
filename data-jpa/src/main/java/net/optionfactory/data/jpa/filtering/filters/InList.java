4f71f1093cf5bb2e7e8571f470c79172cd8891ca
Francesco Komauli <francesco.komauli@optionfactory.net>
2019-11-18 15:06:27 +0100

    :wrench: converting numeric values when filtering by value in list

--- data-jpa/src/main/java/net/optionfactory/data/jpa/filtering/filters/InList.java
+++ data-jpa/src/main/java/net/optionfactory/data/jpa/filtering/filters/InList.java
@@ -17,6 +17,7 @@ import javax.persistence.metamodel.EntityType;
 import net.optionfactory.data.jpa.filtering.filters.InList.InListFilter;
 import net.optionfactory.data.jpa.filtering.filters.InList.RepeatableInList;
 import net.optionfactory.data.jpa.filtering.filters.spi.Filters;
+import org.springframework.util.NumberUtils;
 
 @Documented
 @Target(value = ElementType.TYPE)
@@ -41,11 +42,13 @@ public @interface InList {
 
         private final String name;
         private final String property;
+        private final Class<?> propertyClass;
 
-        public InListFilter(InList il, EntityType<?> entityType) {
-            this.name = il.name();
-            this.property = il.property();
-            Filters.ensurePropertyOfAnyType(il, entityType, property, String.class, Number.class, byte.class, short.class, int.class, long.class, float.class, double.class, char.class);
+        public InListFilter(InList annotation, EntityType<?> entityType) {
+            this.name = annotation.name();
+            this.property = annotation.property();
+            this.propertyClass = entityType.getAttribute(annotation.property()).getJavaType();
+            Filters.ensurePropertyOfAnyType(annotation, entityType, property, String.class, Number.class, byte.class, short.class, int.class, long.class, float.class, double.class, char.class);
         }
 
         @Override
@@ -54,11 +57,40 @@ public @interface InList {
                 return builder.disjunction();
             }
             final Path<?> p = Filters.traverseProperty(root, property);
-            if (Stream.of(values).anyMatch(Objects::isNull)) {
-                final Object[] nonNullValues = Stream.of(values).filter(Objects::nonNull).toArray();
+            final Object[] nonNullValues = Stream.of(values).filter(Objects::nonNull).map(this::convert).toArray();
+            final boolean hasNullValues = nonNullValues.length < values.length;
+            if (hasNullValues) {
                 return builder.or(p.isNull(), p.in(nonNullValues));
             }
-            return p.in((Object[]) values);
+            return p.in(nonNullValues);
+        }
+
+        private Object convert(String value) {
+            if (Number.class.isAssignableFrom(propertyClass)) {
+                return NumberUtils.parseNumber(value, (Class<? extends Number>) propertyClass);
+            }
+            if (byte.class.isAssignableFrom(propertyClass)) {
+                return Byte.parseByte(value);
+            }
+            if (short.class.isAssignableFrom(propertyClass)) {
+                return Short.parseShort(value);
+            }
+            if (int.class.isAssignableFrom(propertyClass)) {
+                return Integer.parseInt(value);
+            }
+            if (long.class.isAssignableFrom(propertyClass)) {
+                return Long.parseLong(value);
+            }
+            if (float.class.isAssignableFrom(propertyClass)) {
+                return Float.parseFloat(value);
+            }
+            if (double.class.isAssignableFrom(propertyClass)) {
+                return Double.parseDouble(value);
+            }
+            if (char.class.isAssignableFrom(propertyClass)) {
+                return value.charAt(0);
+            }
+            return value;
         }
 
         @Override
