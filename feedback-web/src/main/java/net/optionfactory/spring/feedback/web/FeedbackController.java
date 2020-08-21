cc9a90a7327f170a42d1ae13e2337522dbc072f0
Roberto Ferranti <roberto.ferranti@gmail.com>
2020-08-21 15:52:19 +0200

    :new: feedback-web; :new: oidc-authorization-code

---
+++ feedback-web/src/main/java/net/optionfactory/spring/feedback/web/FeedbackController.java
@@ -0,0 +1,43 @@
+package net.optionfactory.spring.feedback.web;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.log4j.Logger;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class FeedbackController<PRINCIPAL> {
+
+    private final Logger logger = Logger.getLogger(FeedbackController.class);
+    private final Function<PRINCIPAL, String> principalToPrefix;
+
+    public FeedbackController(Function<PRINCIPAL, String> principalToPrefix) {
+        this.principalToPrefix = principalToPrefix;
+    }
+    
+    @PostMapping("/client-errors/")
+    public void clientErrors(@AuthenticationPrincipal PRINCIPAL auth, HttpServletRequest req) {
+        logger.warn(String.format("[op:client-error]%s client-error: %s", principalToPrefix.apply(auth), bodyAsLine(req)));
+    }
+
+    @PostMapping("/csp-violations/")
+    public void cspViolation(@AuthenticationPrincipal PRINCIPAL auth, HttpServletRequest req) {
+        logger.warn(String.format("[op:csp-violation]%s csp-violation: %s", principalToPrefix.apply(auth), bodyAsLine(req)));
+    }
+
+    
+    private static final int MAX_BYTES_TO_LOG = 65536;
+
+    private static String bodyAsLine(HttpServletRequest req) {
+        try (final var is = req.getInputStream(); final var baos = new ByteArrayOutputStream()) {
+            StreamUtils.copyRange(is, baos, 0, MAX_BYTES_TO_LOG);
+            return new String(baos.toByteArray(), StandardCharsets.UTF_8).replace("\r", "").replace("\n", " ");
+        } catch (IOException ex) {
+            return "unparseable binary body";
+        }
+    }
+}
---
+++ oidc-authorization-code/src/main/java/net/optionfactory/spring/oidc/authcode/ConfigurableOauth2UserService.java
@@ -0,0 +1,55 @@
+package net.optionfactory.spring.oidc.authcode;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.client.RestTemplate;
+
+/*
+ * so we can customize the RestTemplate
+**/
+public class ConfigurableOauth2UserService<U extends OidcUser> implements OAuth2UserService<OidcUserRequest, OidcUser> {
+
+    private final OidcUserService delegate;
+    private final BiFunction<Set<GrantedAuthority>, OidcUser, U> userFactory;
+
+    public ConfigurableOauth2UserService(HttpComponentsClientHttpRequestFactory httpRequestFactory, ApplicationEventPublisher events, BiFunction<Set<GrantedAuthority>, OidcUser, U> userFactory) {
+        final var oauth2RestTemplate = new RestTemplate(httpRequestFactory);
+        oauth2RestTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+        final var defaultOAuth2UserService = new DefaultOAuth2UserService();
+        defaultOAuth2UserService.setRestOperations(oauth2RestTemplate);
+        this.delegate = new OidcUserService();
+        this.delegate.setOauth2UserService(defaultOAuth2UserService);
+        this.userFactory = userFactory;
+    }
+
+    @Override
+    public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+        final var oidcUser = delegate.loadUser(userRequest);
+        final var augmentedAuthorities = new HashSet<GrantedAuthority>();
+        augmentedAuthorities.addAll(oidcUser.getAuthorities());
+        final List<String> groups = oidcUser.getAttribute("groups");
+        if (groups != null) {
+            final var additionalAuthorities = groups.stream()
+                    .map(g -> String.format("ROLE_GROUP_%s", g.toUpperCase().replace("-", "_")))
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+            augmentedAuthorities.addAll(additionalAuthorities);
+        }
+        return userFactory.apply(augmentedAuthorities, oidcUser);
+    }
+}
