82e8e842989e3fc64e06206295149829c30380f3
Roberto Ferranti <roberto.ferranti@gmail.com>
2021-04-12 19:50:12 +0200

    :new: can now map a principal when using StaticBearerTokenAuthenticationProvider

--- authentication-bearer-token/src/main/java/net/optionfactory/spring/authentication/bearer/token/StaticBearerTokenAuthenticationProvider.java
+++ authentication-bearer-token/src/main/java/net/optionfactory/spring/authentication/bearer/token/StaticBearerTokenAuthenticationProvider.java
@@ -1,6 +1,6 @@
 package net.optionfactory.spring.authentication.bearer.token;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
@@ -13,27 +13,37 @@ import org.springframework.security.core.GrantedAuthority;
  */
 public class StaticBearerTokenAuthenticationProvider implements AuthenticationProvider {
 
-    private final Map<String, ? extends Collection<? extends GrantedAuthority>> tokenToAuthorities;
+    private final Map<String, PrincipalAndAuthorities> tokenToPrincipalAndAuthorities;
 
-    public StaticBearerTokenAuthenticationProvider(String token, Collection<? extends GrantedAuthority> authorities) {
-        this(Map.of(token, authorities));
-    }
-
-    public StaticBearerTokenAuthenticationProvider(Map<String, ? extends Collection<? extends GrantedAuthority>> tokenToAuthorities) {
-        this.tokenToAuthorities = tokenToAuthorities;
+    public StaticBearerTokenAuthenticationProvider(Map<String, PrincipalAndAuthorities> tokenToPrincipalAndAuthorities) {
+        this.tokenToPrincipalAndAuthorities = tokenToPrincipalAndAuthorities;
     }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         final BearerTokenAuthentication bearer = (BearerTokenAuthentication) authentication;
-        if (!tokenToAuthorities.containsKey(bearer.getCredentials())) {
-            return null;
-        }
-        return bearer.makeAuthenticated(null, tokenToAuthorities.get(bearer.getCredentials()));
+        final PrincipalAndAuthorities got = tokenToPrincipalAndAuthorities.get(bearer.getCredentials());
+        return got == null ? null : bearer.makeAuthenticated(got.principal, got.authorities);
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
         return BearerTokenAuthentication.class.isAssignableFrom(authentication);
+    }
+
+    public static StaticBearerTokenAuthenticationProvider of(String token, Object principal, List<GrantedAuthority> authorities) {
+        return new StaticBearerTokenAuthenticationProvider(Map.of(token, new PrincipalAndAuthorities(principal, authorities)));
+    }
+
+    public static class PrincipalAndAuthorities {
+
+        public final Object principal;
+        public final List<GrantedAuthority> authorities;
+
+        public PrincipalAndAuthorities(Object principal, List<GrantedAuthority> authorities) {
+            this.principal = principal;
+            this.authorities = authorities;
+        }
+
     }
 }
