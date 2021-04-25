2aad0af928a7bf41675f3832a4b5c06b4cbdad70
Roberto Ferranti <roberto.ferranti@gmail.com>
2021-04-25 18:57:00 +0200

    :new: helpers in FilterRequest

--- data-jpa/src/main/java/net/optionfactory/spring/data/jpa/filtering/FilterRequest.java
+++ data-jpa/src/main/java/net/optionfactory/spring/data/jpa/filtering/FilterRequest.java
@@ -1,5 +1,6 @@
 package net.optionfactory.spring.data.jpa.filtering;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -11,13 +12,29 @@ public class FilterRequest {
 
     public Map<String, String[]> filters;
 
+    public FilterRequest with(String filter, String... params) {
+        final var fs = new HashMap<>(filters);
+        fs.put(filter, params);
+        return FilterRequest.of(fs);
+    }
+
+    public FilterRequest without(String filter) {
+        final var fs = new HashMap<>(filters);
+        fs.remove(filter);
+        return FilterRequest.of(fs);
+    }
+
+    public static FilterRequest unfiltered() {
+        return of(Map.of());
+    }
+
     public static FilterRequest of(Map<String, String[]> filters) {
         final FilterRequest filterRequest = new FilterRequest();
         filterRequest.filters = filters;
         return filterRequest;
     }
 
-    public static FilterRequest unfiltered() {
-        return of(Map.of());
+    public static FilterRequest of(String filter, String... params) {
+        return of(Map.of(filter, params));
     }
 }
