cec684d967d3e57dabf47137a01fbdf6f4847a4c
Roberto Ferranti <roberto.ferranti@gmail.com>
2022-04-14 11:39:55 +0200

    :new: new dev version 6.5-SNAPSHOT

--- upstream/src/main/java/net/optionfactory/spring/upstream/UpstreamOps.java
+++ upstream/src/main/java/net/optionfactory/spring/upstream/UpstreamOps.java
@@ -3,15 +3,11 @@ package net.optionfactory.spring.upstream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
-import java.util.regex.Pattern;
 import org.springframework.core.io.InputStreamSource;
 import org.springframework.http.MediaType;
 import org.springframework.util.StreamUtils;
 
 public class UpstreamOps {
-
-    private static final Pattern UUID_WITH_ONLY_LETTERS = Pattern.compile("[A-Fa-f]{8}-[A-Fa-f]{4}-[A-Fa-f]{4}-[A-Fa-f]{4}-[A-Fa-f]{12}");
-    private static final Pattern CONTAINS_NUMBERS = Pattern.compile("[0-9]");
 
     private static final Set<String> LOGGED_MEDIA_TYPES = Set.of(
             "JSON",
