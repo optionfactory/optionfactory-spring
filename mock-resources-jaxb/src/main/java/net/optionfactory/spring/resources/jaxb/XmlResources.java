7b9fd79c02a1dd30e0b74ccdb41063816188d082
Roberto Ferranti <roberto.ferranti@gmail.com>
2022-05-24 16:35:28 +0200

    :new: mocks

---
+++ mock-resources-jaxb/src/main/java/net/optionfactory/spring/resources/jaxb/XmlResources.java
@@ -0,0 +1,74 @@
+package net.optionfactory.spring.resources.jaxb;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.transform.stream.StreamSource;
+
+public class XmlResources {
+
+    public enum FallbackStrategy {
+        TRY_DEFAULT, NONE;
+    }
+
+    private final JAXBContext jaxb;
+    private final FallbackStrategy strategy;
+    private final Class<?> baseClass;
+    private final Path basePath;
+
+    public XmlResources(Class<?> jaxbContextClass, FallbackStrategy strategy, Class<?> baseClass, String basePath) {
+        this.jaxb = context(jaxbContextClass);
+        this.strategy = strategy;
+        this.baseClass = baseClass;
+        this.basePath = Path.of(basePath);
+    }
+
+    private static JAXBContext context(Class<?> kl) {
+        try {
+            return JAXBContext.newInstance(kl.getPackageName());
+        } catch (JAXBException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    public <T> T unmarshal(Class<T> type, String prefix, Object... parts) {
+        try ( var is = inputStream(prefix, parts)) {
+            final JAXBElement<T> el = jaxb.createUnmarshaller().unmarshal(new StreamSource(is), type);
+            return el.getValue();
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        } catch (JAXBException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+
+    private InputStream inputStream(String prefix, Object... parts) {
+
+        final var specificSuffix = Stream.of(parts).map(Object::toString).collect(Collectors.joining("_"));
+        final var specificFilename = String.format("%s_%s.json", prefix, specificSuffix);
+        final var specific = basePath.resolve(specificFilename).toString();
+        final var specificIs = baseClass.getResourceAsStream(specific);
+        if (specificIs != null) {
+            return specificIs;
+        }
+        if (strategy == FallbackStrategy.NONE) {
+            throw new IllegalStateException(String.format("no valid resources found for %s: %s", prefix, List.of(specific)));
+        }
+        final var genericFilename = String.format("%s_default.json", prefix);
+        final var generic = basePath.resolve(genericFilename).toString();
+        final var genericIs = baseClass.getResourceAsStream(generic);
+        if (genericIs == null) {
+            throw new IllegalStateException(String.format("no valid resources found for %s: %s", prefix, List.of(specific, generic)));
+        }
+        return genericIs;
+    }
+
+}
