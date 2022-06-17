41d5a15537685b5c32d0152e84880eddb461adc1
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2022-06-17 11:10:36 +0200

    :fix: broken test

--- data-jpa/src/test/java/net/optionfactory/spring/data/jpa/filtering/slicing/SliceTest.java
+++ data-jpa/src/test/java/net/optionfactory/spring/data/jpa/filtering/slicing/SliceTest.java
@@ -1,41 +1,39 @@
 package net.optionfactory.spring.data.jpa.filtering.slicing;
 
 import net.optionfactory.spring.spring.data.jpa.HibernateTestConfig;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = HibernateTestConfig.class)
+@Transactional
 public class SliceTest {
 
+    @Autowired
     private EntityForSliceRepository repo;
-    private TransactionTemplate tx;
 
     @Before
     public void setup() {
-        final AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ctx.register(HibernateTestConfig.class);
-        ctx.refresh();
-        this.repo = ctx.getBean(EntityForSliceRepository.class);
-        this.tx = ctx.getBean(TransactionTemplate.class);
-        tx.execute(txs -> {
-            repo.deleteAll();
-            final EntityForSlice a = new EntityForSlice();
-            a.id = 123;
-            a.name = "asd";
-            repo.save(a);
-            return null;
-        });
+        repo.deleteAll();
+        final EntityForSlice a = new EntityForSlice();
+        a.id = 123;
+        a.name = "asd";
+        repo.save(a);
     }
 
     @Test
     public void textSlice() {
-        
         Pageable p = PageRequest.of(0, 100);
         Slice<EntityForSlice> findByName = repo.findByName("asd", p);
-        findByName.hasPrevious();
+        Assert.assertFalse(findByName.hasPrevious());
     }
 }
