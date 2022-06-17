89a331fd4e30b336d366a3cf3fcd2ea2ec3d34b6
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2022-06-17 11:25:23 +0200

    :white_check_mark: add some basic tests

--- data-jpa/src/test/java/net/optionfactory/spring/data/jpa/filtering/slicing/SliceTest.java
+++ data-jpa/src/test/java/net/optionfactory/spring/data/jpa/filtering/slicing/SliceTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,19 +22,45 @@ public class SliceTest {
     @Autowired
     private EntityForSliceRepository repo;
 
+    private EntityForSlice entity(long id, String name) {
+        final var entity = new EntityForSlice();
+        entity.id = id;
+        entity.name = name;
+        return entity;
+    }
+
     @Before
     public void setup() {
         repo.deleteAll();
-        final EntityForSlice a = new EntityForSlice();
-        a.id = 123;
-        a.name = "asd";
-        repo.save(a);
+        repo.save(entity(1, "TEST"));
+        repo.save(entity(2, "TEST"));
+        repo.save(entity(3, "TEST"));
+        repo.save(entity(4, "TEST"));
+        repo.save(entity(5, "TEST"));
+        repo.save(entity(6, "TEST"));
     }
 
     @Test
-    public void textSlice() {
-        Pageable p = PageRequest.of(0, 100);
-        Slice<EntityForSlice> findByName = repo.findByName("asd", p);
+    public void firstSliceHasNoPrevious() {
+        Pageable p = PageRequest.of(0, 2, Sort.by("id"));
+        Slice<EntityForSlice> findByName = repo.findByName("TEST", p);
         Assert.assertFalse(findByName.hasPrevious());
+        Assert.assertTrue(findByName.hasContent());
+    }
+
+    @Test
+    public void firstSliceHasNext() {
+        Pageable p = PageRequest.of(0, 2, Sort.by("id"));
+        Slice<EntityForSlice> findByName = repo.findByName("TEST", p);
+        Assert.assertTrue(findByName.hasNext());
+        Assert.assertTrue(findByName.hasContent());
+    }
+
+    @Test
+    public void secondSliceHasPrevious() {
+        Pageable p = PageRequest.of(1, 2, Sort.by("id"));
+        Slice<EntityForSlice> findByName = repo.findByName("TEST", p);
+        Assert.assertTrue(findByName.hasPrevious());
+        Assert.assertTrue(findByName.hasContent());
     }
 }
