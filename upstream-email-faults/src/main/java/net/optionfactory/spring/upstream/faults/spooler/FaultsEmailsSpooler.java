c0253496f02be14b987dfe6c228f3fd057d404a4
Roberto Ferranti <roberto.ferranti@gmail.com>
2022-12-12 13:51:45 +0100

    :ref: simplified faults emails spooler

--- upstream-email-faults/src/main/java/net/optionfactory/spring/upstream/faults/spooler/FaultsEmailsSpooler.java
+++ upstream-email-faults/src/main/java/net/optionfactory/spring/upstream/faults/spooler/FaultsEmailsSpooler.java
@@ -1,55 +1,49 @@
 package net.optionfactory.spring.upstream.faults.spooler;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.ReentrantLock;
 import net.optionfactory.spring.email.EmailMessage;
+import net.optionfactory.spring.email.EmailPaths;
 import net.optionfactory.spring.email.EmailSenderAndCopyAddresses;
 import net.optionfactory.spring.email.marshaller.EmailMarshaller;
-import net.optionfactory.spring.problems.Problem;
-import net.optionfactory.spring.problems.Result;
 import net.optionfactory.spring.upstream.UpstreamFaultsSpooler;
 import net.optionfactory.spring.upstream.UpstreamFaultsSpooler.UpstreamFault;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.Assert;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 public class FaultsEmailsSpooler<T> implements UpstreamFaultsSpooler<T> {
 
-    
     private final Logger logger = LoggerFactory.getLogger(FaultsEmailsSpooler.class);
-    
+
     private final EmailMarshaller emailMarshaller = new EmailMarshaller();
     private final EmailSenderAndCopyAddresses senderAndCopyAddresses;
     private final SubjectTemplateConfiguration emailSubjects;
     private final TemplateEngine emailTemplates;
     private final TemplateEngine stringTemplates;
     private final String emailTemplateName;
-    private final Path emailSpoolDirectory;
+    private final EmailPaths paths;
     private final String recipient;
     private final ConcurrentLinkedQueue<UpstreamFault<T>> faults;
 
     private final AtomicReference<Instant> lastFaultSpool = new AtomicReference<>(Instant.EPOCH);
-    private final AtomicReference<Duration> gracePeriod = new AtomicReference<>(Duration.ofMinutes(5));    
+    private final AtomicReference<Duration> gracePeriod = new AtomicReference<>(Duration.ofMinutes(5));
 
-    public FaultsEmailsSpooler(EmailSenderAndCopyAddresses emailSenderConfiguration, SubjectTemplateConfiguration emailSubjects, TemplateEngine emailTemplates, TemplateEngine stringTemplates, String emailTemplateName, Path emailSpoolDirectory, String recipient) {
-        Assert.isTrue(Files.isDirectory(emailSpoolDirectory), "emailSpoolDirectory must be a directory");
-        Assert.isTrue(Files.isWritable(emailSpoolDirectory), "emailSpoolDirectory must be writable");
+    public FaultsEmailsSpooler(EmailSenderAndCopyAddresses emailSenderConfiguration, SubjectTemplateConfiguration emailSubjects, TemplateEngine emailTemplates, TemplateEngine stringTemplates, String emailTemplateName, EmailPaths paths, String recipient) {
         this.senderAndCopyAddresses = emailSenderConfiguration;
         this.emailSubjects = emailSubjects;
         this.emailTemplates = emailTemplates;
         this.stringTemplates = stringTemplates;
         this.emailTemplateName = emailTemplateName;
-        this.emailSpoolDirectory = emailSpoolDirectory;
+        this.paths = paths;
         this.recipient = recipient;
         this.faults = new ConcurrentLinkedQueue<>();
     }
@@ -59,30 +53,25 @@ public class FaultsEmailsSpooler<T> implements UpstreamFaultsSpooler<T> {
         faults.add(fault);
     }
 
-    public synchronized void spool() {
-        var now = Instant.now();
-        if (Duration.between(lastFaultSpool.get(), now).compareTo(gracePeriod.get()) < 0) {
-            //max one email every grace period
-            return;
-        }
-        final Result<Path> spooled = dumpToEml();
-        if (spooled.isError()) {
-            logger.warn("[spool-emails][faults] failed to dump email: {}", spooled.getErrors());
-            return;
-        }
-        if (spooled.getValue() == null) {
-            //success, but no cigar
-            return;
-        }
-        logger.info("[spool-emails][faults] spooled {}", spooled.getValue().getFileName());
-        lastFaultSpool.set(now);
-    }
+    private final ReentrantLock spoolLock = new ReentrantLock();
 
-    private Result<Path> dumpToEml() {
+    public void spool() {
         try {
+            if (!spoolLock.tryLock(1, TimeUnit.SECONDS)) {
+                return;
+            }
+        } catch (InterruptedException ex) {
+            return;
+        }
+        try {
+            var now = Instant.now();
+            if (Duration.between(lastFaultSpool.get(), now).compareTo(gracePeriod.get()) < 0) {
+                //max one email every grace period
+                return;
+            }
             final List<UpstreamFault<T>> batch = drain();
-            if(batch.isEmpty()){
-                return Result.value(null);
+            if (batch.isEmpty()) {
+                return;
             }
             final var message = new EmailMessage();
             final var context = new Context();
@@ -93,22 +82,21 @@ public class FaultsEmailsSpooler<T> implements UpstreamFaultsSpooler<T> {
             message.textBody = null;
             message.messageId = Long.toString(Instant.now().toEpochMilli());
             message.recipient = recipient;
-
-            final Path tempPath = Files.createTempFile(emailSpoolDirectory, "faults.", ".tmp");
-            emailMarshaller.marshal(senderAndCopyAddresses, message, List.of(), List.of(), tempPath);
-            final Path targetPath = emailSpoolDirectory.resolve(tempPath.getFileName().toString().replace(".tmp", ".eml"));
-            Files.move(tempPath, targetPath, StandardCopyOption.ATOMIC_MOVE);
+            final Path p = emailMarshaller.marshalToSpool(senderAndCopyAddresses, message, List.of(), List.of(), paths, "faults.");
             faults.clear();
-            return Result.value(targetPath);
-        } catch (IOException ex) {
-            return Result.error(Problem.of("SPOOLING_ERROR", null, null, ex.getMessage()));
+            lastFaultSpool.set(now);
+            logger.info("[spool-emails][faults] spooled {}", p.getFileName());
+        } catch (Exception ex) {
+            logger.warn("[spool-emails][faults] failed to dump email", ex);
+        } finally {
+            spoolLock.unlock();
         }
     }
 
     private List<UpstreamFault<T>> drain() {
         final List<UpstreamFault<T>> batch = new ArrayList<>();
         UpstreamFault<T> fault;
-        while((fault = faults.poll()) != null){
+        while ((fault = faults.poll()) != null) {
             batch.add(fault);
         }
         return batch;
