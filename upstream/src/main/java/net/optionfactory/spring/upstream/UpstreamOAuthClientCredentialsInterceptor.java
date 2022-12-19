42b068ffa957c319076e82cea972cc1052a826eb
AndreaOddo89 <andrea.oddo@optionfactory.net>
2022-12-19 12:07:08 +0100

    :ref: changed access modifier to allow UpstreamOauthClientCredentialsInterceptor extending classes to override getAuthToken

--- upstream/src/main/java/net/optionfactory/spring/upstream/UpstreamOAuthClientCredentialsInterceptor.java
+++ upstream/src/main/java/net/optionfactory/spring/upstream/UpstreamOAuthClientCredentialsInterceptor.java
@@ -49,7 +49,7 @@ public class UpstreamOAuthClientCredentialsInterceptor<T> implements UpstreamInt
         return headers;
     }
 
-    private String getOauthToken() throws RestClientException {
+    protected String getOauthToken() throws RestClientException {
         final var headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON_UTF8));
         headers.setContentType(MediaType.valueOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8"));
