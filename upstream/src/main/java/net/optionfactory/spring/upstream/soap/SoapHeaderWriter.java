3fc0ebc9b11332d5af252ffb1ba12db1f8288983
Roberto Ferranti <roberto.ferranti@gmail.com>
2023-11-15 17:02:43 +0100

    :new: SoapHeaderWriter.WssUsernameToken

--- upstream/src/main/java/net/optionfactory/spring/upstream/soap/SoapHeaderWriter.java
+++ upstream/src/main/java/net/optionfactory/spring/upstream/soap/SoapHeaderWriter.java
@@ -1,10 +1,43 @@
 package net.optionfactory.spring.upstream.soap;
 
+import jakarta.xml.soap.SOAPException;
 import jakarta.xml.soap.SOAPHeader;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import javax.xml.namespace.QName;
 
 public interface SoapHeaderWriter {
 
     public static SoapHeaderWriter NONE = null;
 
     public void write(SOAPHeader header);
+
+    public static class WssUsernameToken implements SoapHeaderWriter {
+
+        public static final String WSU_NAMESPACE_URI = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
+        public static final String WSSE_NAMESPACE_URI = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
+        public static final String PASSWORD_TYPE_TEXT_ATTRIBUTE_VALUE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText";
+        private final String username;
+        private final String password;
+
+        public WssUsernameToken(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        @Override
+        public void write(SOAPHeader header) {
+            try {
+                final var usernameTokenId = String.format("UsernameToken-%s", UUID.nameUUIDFromBytes(String.format("%s:%s", username, password).getBytes(StandardCharsets.UTF_8)));
+                final var sec = header.addChildElement("Security", "wsse", WSSE_NAMESPACE_URI).addNamespaceDeclaration("wsu", WSU_NAMESPACE_URI);
+                sec.addAttribute(new QName(header.getElementQName().getNamespaceURI(), "mustUnderstand", header.getElementQName().getPrefix()), "1");
+                final var token = sec.addChildElement("UsernameToken", "wsse").addAttribute(new QName(WSU_NAMESPACE_URI, "Id", "wsu"), usernameTokenId);
+                token.addChildElement("Username", "wsse").addTextNode(username);
+                token.addChildElement("Password", "wsse").addAttribute(new QName("Type"), PASSWORD_TYPE_TEXT_ATTRIBUTE_VALUE).addTextNode(password);
+            } catch (SOAPException ex) {
+                throw new IllegalStateException(ex);
+            }
+
+        }
+    }
 }
---
+++ upstream/src/test/java/net/optionfactory/spring/upstream/soap/SoapHeaderWriterTest.java
@@ -0,0 +1,47 @@
+package net.optionfactory.spring.upstream.soap;
+
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.soap.MessageFactory;
+import jakarta.xml.soap.SOAPException;
+import jakarta.xml.soap.SOAPMessage;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import net.optionfactory.spring.upstream.rendering.BodyRendering;
+import net.optionfactory.spring.upstream.soap.SoapJaxbHttpMessageConverter.Protocol;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.util.FastByteArrayOutputStream;
+
+public class SoapHeaderWriterTest {
+
+    @Test
+    public void canSerializeSoapHeaderUsingWssUsernameToken() throws JAXBException, SOAPException, IOException {
+        final var baos = new FastByteArrayOutputStream();
+        final var messageFactory = MessageFactory.newInstance(Protocol.SOAP_1_1.value);
+        final var hw = new SoapHeaderWriter.WssUsernameToken("username", "password");
+        final var soapMessage = messageFactory.createMessage();
+
+        hw.write(soapMessage.getSOAPHeader());
+        soapMessage.saveChanges();
+        soapMessage.writeTo(baos);
+
+        final var expected = BodyRendering.xsltCompact("""
+                            <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+                                <SOAP-ENV:Header>
+                                    <wsse:Security 
+                                        xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" 
+                                        xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" 
+                                        SOAP-ENV:mustUnderstand="1"
+                                    >
+                                        <wsse:UsernameToken wsu:Id="UsernameToken-133e1b8e-da33-3c4c-bf7a-508620ca7f10">
+                                            <wsse:Username>username</wsse:Username>
+                                            <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">password</wsse:Password>
+                                        </wsse:UsernameToken>
+                                    </wsse:Security>
+                                </SOAP-ENV:Header>
+                                <SOAP-ENV:Body/>
+                            </SOAP-ENV:Envelope>""".getBytes(StandardCharsets.UTF_8));
+
+        Assert.assertEquals(expected, new String(baos.toByteArrayUnsafe(), StandardCharsets.UTF_8));
+    }
+}
