cd9e40775a0c42ed867bcaaf8332986930990a19
Roberto Ferranti <roberto.ferranti@gmail.com>
2023-11-23 19:30:34 +0100

    :ref: reused ClientHttpRequestFactory in UpstreamDigestAuthenticationInterceptor

--- upstream/src/main/java/net/optionfactory/spring/upstream/auth/digest/UpstreamDigestAuthenticationInterceptor.java
+++ upstream/src/main/java/net/optionfactory/spring/upstream/auth/digest/UpstreamDigestAuthenticationInterceptor.java
@@ -2,34 +2,39 @@ package net.optionfactory.spring.upstream.auth.digest;
 
 import java.io.IOException;
 import net.optionfactory.spring.upstream.UpstreamHttpInterceptor;
-import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClient;
 
 public class UpstreamDigestAuthenticationInterceptor implements UpstreamHttpInterceptor {
 
     private final DigestAuth digestAuth;
-    private final CloseableHttpClient authClient;
+    private RestClient restClient;
 
-    public UpstreamDigestAuthenticationInterceptor(String clientId, String clientSecret, CloseableHttpClient authClient) {
+    public UpstreamDigestAuthenticationInterceptor(String clientId, String clientSecret) {
         this.digestAuth = DigestAuth.fromCredentials(clientId, clientSecret);
-        this.authClient = authClient;
+    }
+
+    @Override
+    public void preprocess(Class<?> k, ClientHttpRequestFactory rf) {
+        this.restClient = RestClient.builder()
+                .requestFactory(rf)
+                .build();
     }
 
     @Override
     public ClientHttpResponse intercept(InvocationContext ctx, HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-        try {
-            final String challenge = authClient.execute(
-                    new HttpPost(request.getURI()),
-                    response -> response.getFirstHeader("WWW-Authenticate").getValue()
-            );
-            request.getHeaders().set("Authorization", digestAuth.authHeader(request.getMethod().name(), request.getURI().getPath(), challenge));
-        } catch (IOException | RuntimeException ex) {
-            throw new RestClientException("Authentication failed", ex);
-        }
+        final var challenge = restClient.method(HttpMethod.POST)
+                .uri(request.getURI())
+                .retrieve()
+                .toBodilessEntity()
+                .getHeaders()
+                .getFirst("WWW-Authenticate");
+
+        request.getHeaders().set("Authorization", digestAuth.authHeader(request.getMethod().name(), request.getURI().getPath(), challenge));
         return execution.execute(request, body);
     }
 
