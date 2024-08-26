0b190122e1ffcba91e10d802d23fbe751f4a46b8
Roberto Ferranti <roberto.ferranti@gmail.com>
2024-08-26 15:05:45 +0200

    :ref: StrictContentSecurityPolicyHandlerInterceptor skips adding cspnonce to redirect views

--- content-security-policies/src/main/java/net/optionfactory/spring/csp/StrictContentSecurityPolicyHandlerInterceptor.java
+++ content-security-policies/src/main/java/net/optionfactory/spring/csp/StrictContentSecurityPolicyHandlerInterceptor.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 public class StrictContentSecurityPolicyHandlerInterceptor implements HandlerInterceptor {
 
@@ -12,7 +13,17 @@ public class StrictContentSecurityPolicyHandlerInterceptor implements HandlerInt
         if (mav == null) {
             return;
         }
+        if (isRedirectView(mav)) {
+            return;
+        }
         mav.addObject("cspnonce", request.getAttribute("cspnonce"));
     }
 
+    private boolean isRedirectView(ModelAndView mav) {
+        if (mav.getView() instanceof RedirectView) {
+            return true;
+        }
+        final var vn = mav.getViewName();
+        return vn != null && vn.startsWith("redirect:");
+    }
 }
