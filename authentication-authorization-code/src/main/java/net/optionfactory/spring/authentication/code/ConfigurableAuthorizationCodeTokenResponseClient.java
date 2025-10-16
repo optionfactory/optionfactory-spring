0b5e3a4be7ff5859fb5633413f599715ea58c8cf
Roberto Ferranti <roberto.ferranti@gmail.com>
2025-10-16 14:52:57 +0200

    :ref: removed use of DefaultAuthorizationCodeTokenResponseClient, replaced with the new RestClientAuthorizationCodeTokenResponseClient

--- authentication-authorization-code/src/main/java/net/optionfactory/spring/authentication/code/ConfigurableAuthorizationCodeTokenResponseClient.java
+++ authentication-authorization-code/src/main/java/net/optionfactory/spring/authentication/code/ConfigurableAuthorizationCodeTokenResponseClient.java
@@ -1,24 +1,31 @@
 package net.optionfactory.spring.authentication.code;
 
-import java.util.List;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.endpoint.RestClientAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
 import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 public class ConfigurableAuthorizationCodeTokenResponseClient implements OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> {
 
-    private final DefaultAuthorizationCodeTokenResponseClient inner;
+    private final RestClientAuthorizationCodeTokenResponseClient inner;
 
     public ConfigurableAuthorizationCodeTokenResponseClient(ClientHttpRequestFactory httpRequestFactory) {
-        final var accessTokenRestTemplate = new RestTemplate(List.of(new FormHttpMessageConverter(), new OAuth2AccessTokenResponseHttpMessageConverter()));
-        accessTokenRestTemplate.setRequestFactory(httpRequestFactory);
-        this.inner = new DefaultAuthorizationCodeTokenResponseClient();
-        this.inner.setRestOperations(accessTokenRestTemplate);
+	final var restClient = RestClient.builder()
+			.messageConverters((messageConverters) -> {
+				messageConverters.clear();
+				messageConverters.add(new FormHttpMessageConverter());
+				messageConverters.add(new OAuth2AccessTokenResponseHttpMessageConverter());
+			})
+			.defaultStatusHandler(new OAuth2ErrorResponseErrorHandler())
+                        .requestFactory(httpRequestFactory)
+			.build();
+        this.inner = new RestClientAuthorizationCodeTokenResponseClient();
+        this.inner.setRestClient(restClient);
     }
 
     @Override
