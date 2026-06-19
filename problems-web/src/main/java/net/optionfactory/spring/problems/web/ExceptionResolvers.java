1afd003ed6ebe7b0c38d0d7fba79b2ffa7456894
Roberto Ferranti <roberto.ferranti@gmail.com>
2026-06-19 14:49:04 +0200

    :ref: ExceptionResolvers order

--- problems-web/src/main/java/net/optionfactory/spring/problems/web/ExceptionResolvers.java
+++ problems-web/src/main/java/net/optionfactory/spring/problems/web/ExceptionResolvers.java
@@ -3,6 +3,7 @@ package net.optionfactory.spring.problems.web;
 import java.util.List;
 import java.util.function.Consumer;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 import tools.jackson.databind.json.JsonMapper;
 
 public class ExceptionResolvers {
@@ -45,9 +46,35 @@ public class ExceptionResolvers {
         return this;
     }
 
+    /// Places the configured exception resolvers in the chain, in this order:
+    ///   - RestExceptionResolver: on top 
+    ///   - BinaryResponseExceptionResolver: on top, after the
+    ///     RestExceptionResolver if it's configured'
+    ///   - PagesExceptionResolver: after ExceptionHandlerExceptionResolver
+    ///     if it's confifgured, on top after BinaryResponseExceptionResolver
+    ///     otherwhise'(if it's configured'))
+    ///
+    ///  By default, if they are all configured the chain looks like this:
+    ///
+    /// | handler | notes  |
+    /// | ------- | ------ |
+    /// | `RestExceptionResolver` |  |
+    /// | `BinaryResponseExceptionResolver` | |
+    /// | `ExceptionHandlerExceptionResolver` | handles `@ExceptionHandler`, but only for pages |
+    /// | `PagesExceptionResolver` |  |
+    /// | `ResponseStatusExceptionResolver` | generally useless, handled by the other resolvers if configured |
+    /// | `AccessDeniedExceptionResolver` | rethrows `AccessDeniedException`s so they can be handled by the `ExceptionTranslationFilter` |
+    /// | `DefaultHandlerExceptionResolver` | generally useless, handled by other resolvers if configured |
     public void configure() {
+        int eherIndex = -1;
+        for (int i = 0; i < container.size(); i++) {
+            if (container.get(i) instanceof ExceptionHandlerExceptionResolver) {
+                eherIndex = i;
+                break;
+            }
+        }
         if (pages != null) {
-            container.addFirst(pages);
+            container.add(eherIndex + 1, pages);
         }
         if (binaries != null) {
             container.addFirst(binaries);
