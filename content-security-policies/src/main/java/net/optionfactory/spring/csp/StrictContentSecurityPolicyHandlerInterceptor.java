02d874fa28622accf29c3f230d45ff11be2f130f
Roberto Ferranti <roberto.ferranti@gmail.com>
2026-07-19 11:58:45 +0200

    :ref: check for redirect view in StrictContentSecurityPolicyHandlerInterceptor

--- content-security-policies/src/main/java/net/optionfactory/spring/csp/StrictContentSecurityPolicyHandlerInterceptor.java
+++ content-security-policies/src/main/java/net/optionfactory/spring/csp/StrictContentSecurityPolicyHandlerInterceptor.java
@@ -4,15 +4,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.SmartView;
 
 public class StrictContentSecurityPolicyHandlerInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView mav) throws Exception {
-        if (mav == null) {
+        if (mav == null | isRedirect(mav)) {
             return;
         }
         mav.addObject("csp", request.getAttribute("csp"));
+    }
+
+    private boolean isRedirect(ModelAndView mav) {
+        if (mav.getViewName() != null && mav.getViewName().startsWith("redirect:")) {
+            return true;
+        }
+        return mav.getView() instanceof SmartView smartView && smartView.isRedirectView();
     }
 
 }
